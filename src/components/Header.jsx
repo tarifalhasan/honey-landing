ed5aeb97498e5f23ff60554a13c8409b20e8efbe
tarifalhasan <tarifalhasan999@gmail.com>
2024-11-20 08:24:42 +0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,10 @@ const Header = () => {
   return (
     <header className="flex items-center py-5 justify-between container">
       <div className="inline-flex items-center gap-4">
-        <h4
-          style={{
-            textShadow: `
+        <a href="/">
+          <h4
+            style={{
+              textShadow: `
         3px 3px 0 #2d3436,
         -1px -1px 0 #2d3436,  
         1px -1px 0 #2d3436,
@@ -15,11 +16,12 @@ const Header = () => {
         1px 1px 0 #2d3436,
         4px 4px 8px rgba(0,0,0,0.3)
       `,
-          }}
-          className="text-[#F5C11E] font-comicBold text-2xl lg:text-5xl"
-        >
-          Honey
-        </h4>
+            }}
+            className="text-[#F5C11E] font-comicBold text-2xl lg:text-5xl"
+          >
+            Honey
+          </h4>
+        </a>
         <ul className="inline-flex items-center gap-2 mt-2.5">
           <a href="http://" target="_blank" rel="noopener noreferrer">
             <FaTelegramPlane className="text-3xl hover:text-blue-500 transition-colors duration-700 text-[red] xl:text-5xl" />
